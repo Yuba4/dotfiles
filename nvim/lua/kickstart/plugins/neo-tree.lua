3e8d36bcb880473710bd76bde2eedb6177f78252
Hiroto Yuba <abyu629@gmail.com>
2026-07-23 22:15:01 +0900

    feat: neo-treeにNerdフォントアイコンを追加

--- nvim/lua/kickstart/plugins/neo-tree.lua
+++ nvim/lua/kickstart/plugins/neo-tree.lua
@@ -10,7 +10,31 @@ vim.pack.add {
 vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', silent = true })
 
 require('neo-tree').setup {
+  default_component_configs = {
+    icon = {
+      folder_closed = '',
+      folder_open = '',
+      folder_empty = '󰜌',
+      default = '󰈔',
+    },
+    git_status = {
+      symbols = {
+        added = '✚',
+        deleted = '✖',
+        modified = '',
+        renamed = '󰁕',
+        untracked = '',
+        ignored = '',
+        unstaged = '󰄱',
+        staged = '',
+        conflict = '',
+      },
+    },
+  },
   filesystem = {
+    filtered_items = {
+      hide_dotfiles = false,
+    },
     window = {
       mappings = {
         ['\\'] = 'close_window',
@@ -18,3 +42,5 @@ require('neo-tree').setup {
     },
   },
 }
+
+vim.api.nvim_create_autocmd('VimEnter', { command = 'Neotree show' })
