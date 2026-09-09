4e26d8aea2cd1dad82a2a3b6645287ff765799bd
Hiroto Yuba <abyu629@gmail.com>
2026-09-09 19:26:45 +0900

    refactor: nvimの設定をkickstart.nvimからLazyVimに移行する

--- nvim/lua/plugins/codediff.lua
+++
@@ -1,5 +0,0 @@
-local gh = require('util').gh
-
-vim.pack.add { gh 'esmuellert/codediff.nvim' }
-
-vim.keymap.set('n', '<leader>gd', '<cmd>CodeDiff<cr>', { desc = '[G]it: Open Code[D]iff' })
--- nvim/lua/util.lua
+++
@@ -1,9 +0,0 @@
-local M = {}
-
----Because most plugins are hosted on GitHub, you can use the helper
----function to have less repetition across plugin config files.
----@param repo string
----@return string
-function M.gh(repo) return 'https://github.com/' .. repo end
-
-return M
